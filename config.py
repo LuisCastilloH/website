adeb49f3c40a76a484bfa1f7f21f9368dbcfa38b
LuisCastilloH <andres.castillo.hdez@gmail.com>
2018-03-22 01:49:02 -0600

    Pagination fixed

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['luis.castillo.h@outlook.com']
+    POSTS_PER_PAGE = 25 
