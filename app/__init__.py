4864c91cb2fee192c6f3ae203c6ed0a6a9f0334a
LuisCastilloH <andres.castillo.hdez@gmail.com>
2018-03-29 03:01:06 -0600

    frontend

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+# from flask_material import Material
+from flask_bootstrap import Bootstrap
 
 import logging
 from logging.handlers import SMTPHandler
@@ -17,6 +19,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+# material = Material(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
