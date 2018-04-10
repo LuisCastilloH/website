f62e69c358bb34a2f65632eb28b47686f09cb3bd
LuisCastilloH <andres.castillo.hdez@gmail.com>
2018-04-10 04:42:11 -0500

    Ajax

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from flask import g
+from flask import jsonify
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user
 from flask_login import logout_user
@@ -8,6 +9,7 @@ from flask_login import login_required
 from flask_babel import _
 from flask_babel import get_locale
 from werkzeug.urls import url_parse
+from guess_language import guess_language
 
 from app import app
 from app import db
@@ -20,6 +22,7 @@ from app.forms import ResetPasswordForm
 from app.email import send_password_reset_email
 from app.models import User
 from app.models import Post
+from app.translate import translate
 
 @app.before_request
 def before_request():
@@ -34,7 +37,11 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -188,4 +195,10 @@ def reset_password(token):
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
 
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
 
